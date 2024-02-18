e7b504d935eb34c35b433efcdde61904dab8447c
HARSHITH13 <harshithcm15@gmail.com>
2024-02-18 23:13:47 +0530

    Added Basket Microservice

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        // Represents distributed cache of searlized values
+        private readonly IDistributedCache _rediscache;
+
+        public BasketRepository(IDistributedCache rediscache)
+        {
+            _rediscache = rediscache ?? throw new ArgumentNullException(nameof(rediscache));
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        { 
+            var basket = await _rediscache.GetStringAsync(username);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        { 
+            await _rediscache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string username)
+        { 
+            await _rediscache.RemoveAsync(username);
+        }
+    }
+}
